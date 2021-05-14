272410f4b008b3bb3e88feeb06cf9a1d4a65b46e
Jesse White <jesse.white6@gmail.com>
2021-05-14 10:29:20 -0700

    add route for new order within a current vendor.

--- PierresBakeryInc/Controllers/VendorController.cs
+++ PierresBakeryInc/Controllers/VendorController.cs
@@ -34,5 +34,17 @@ namespace PierresBakeryInc.Controllers
       model.Add("orders", vendorOrders);
       return View(model);
     }
+    [HttpPost("/vendors/{vendorId}/orders")]
+    public ActionResult Create(int vendorId, string orderItem)
+    {
+      Dictionary<string, object> model = new Dictionary<string, object>();
+      Vendor foundVendor = Vendor.Find(vendorId);
+      Order newOrder = new Order(orderItem);
+      foundVendor.AddOrder(newOrder);
+      List<Order> vendorOrders = foundVendor.Orders;
+      model.Add("orders", vendorOrders);
+      model.Add("vendor", foundVendor);
+      return View("Show", model);
+    }
   }
 }
