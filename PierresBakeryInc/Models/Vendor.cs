d3ba8c3cf74695719aed8ed52f3db4697583962e
Jesse White <jesse.white6@gmail.com>
2021-05-14 10:02:47 -0700

    pass all failing tests.

--- PierresBakeryInc/Models/Vendor.cs
+++ PierresBakeryInc/Models/Vendor.cs
@@ -7,7 +7,7 @@ namespace PierresBakeryInc.Models
     private static List<Vendor> _instances = new List<Vendor> { };
     public string VendorName { get; set; }
     public int Id { get; }
-    public List<Vendor> Vendors { get; set; }
+    public List<Order> Orders { get; set; }
 
     public static void ClearAll()
     {
@@ -18,7 +18,7 @@ namespace PierresBakeryInc.Models
       VendorName = vendorName;
       _instances.Add(this);
       Id = _instances.Count;
-      Order = new List<Order> { };
+      Orders = new List<Order> { };
     }
     public static List<Vendor> GetAll()
     {
@@ -27,6 +27,10 @@ namespace PierresBakeryInc.Models
     public static Vendor Find(int searchId)
     {
       return _instances[searchId - 1];
+    }
+    public void AddOrder(Order order)
+    {
+      Orders.Add(order);
     }
   }
 }
